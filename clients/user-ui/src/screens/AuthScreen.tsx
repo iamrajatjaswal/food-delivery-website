8da9d43b35a30b9c418c822078e2410949e06506
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-12-12 01:35:12 +0530

    UPDATE Login.tsx & AuthScreen.tsx : updated the UI

--- clients/user-ui/src/screens/AuthScreen.tsx
+++ clients/user-ui/src/screens/AuthScreen.tsx
@@ -3,7 +3,7 @@ import Login from "../shared/Auth/Login";
 const AuthScreen = () => {
   return (
     <div className="w-full fixed top-0 left-0 h-screen z-50 flex items-center justify-center bg-[#00000027]">
-      <div className="w-[500px] h-[500px] bg-slate-900 rounded shadow-sm p-3">
+      <div className="w-[500px] bg-slate-900 rounded shadow-sm p-3">
         <Login />
       </div>
     </div>
--- clients/user-ui/src/shared/Auth/Login.tsx
+++ clients/user-ui/src/shared/Auth/Login.tsx
@@ -2,8 +2,13 @@ import styles from "@/src/utils/style";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { AiFillGithub } from "react-icons/ai";
+import {
+  AiFillGithub,
+  AiOutlineEye,
+  AiOutlineEyeInvisible,
+} from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
+import { useState } from "react";
 
 const formSchema = z.object({
   email: z.string().email(),
@@ -22,6 +27,8 @@ const Login = () => {
     resolver: zodResolver(formSchema),
   });
 
+  const [show, setShow] = useState(false);
+
   const onSubmit = (data: LoginSchema) => {
     console.log(data);
     reset();
@@ -29,7 +36,6 @@ const Login = () => {
 
   return (
     <div>
-      <br />
       <h1 className={`${styles.title}`}>Login with Code Academy</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="w-full mt-5 relative mb-1">
@@ -55,19 +61,30 @@ const Login = () => {
           </label>
           <input
             {...register("password")}
-            type="password"
+            type={!show ? "password" : "text"}
             placeholder="password!@%"
             className={`${styles.input}`}
           />
           {errors.password && (
-            <span className="text-red-500 block mt-1">
-              {errors.password.message}
-            </span>
+            <span className="text-red-500">{errors.password.message}</span>
+          )}
+          {show ? (
+            <AiOutlineEyeInvisible
+              className="absolute bottom-3 right-2 z-1 cursor-pointer"
+              size={20}
+              onClick={() => setShow(!show)}
+            />
+          ) : (
+            <AiOutlineEye
+              className="absolute bottom-3 right-2 z-1 cursor-pointer"
+              size={20}
+              onClick={() => setShow(!show)}
+            />
           )}
         </div>
         <div className="w-full mt-5">
           <span
-            className={`${styles.label} py-2 text-[#4740ca] block text-right cursor-pointer`}
+            className={`${styles.label} text-[#2190ff] block text-right cursor-pointer`}
           >
             Forgot your password?
           </span>
@@ -79,16 +96,18 @@ const Login = () => {
           />
         </div>
         <br />
-        <h5 className="text-center pt-4 font-Poppins text-[14px] text-white">
+        <h5 className="text-center pt-4 font-Poppins text-[16px] text-white">
           Or join with
         </h5>
         <div className="flex items-center justify-center my-3">
           <FcGoogle size={30} className="cursor-pointer mr-2" />
-          <AiFillGithub size={30} className="cursor-pointer mr-2" />
+          <AiFillGithub size={30} className="cursor-pointer ml-2" />
         </div>
         <h5 className="text-center pt-4 font-Poppins text-[14px]">
           Not have any account?
+          <span className="text-[#2190ff] pl-1 cursor-pointer">Sign up</span>
         </h5>
+        <br />
       </form>
     </div>
   );
